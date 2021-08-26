0df458d92acccf61cef5abc1e0d5697ea1437aa4
Sihle <sihle@thedigitalacademy.co.za>
2021-08-26 17:22:37 +0200

    Connecting Mongo Dcoker Container from Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.API.Data
+{
+    internal class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
